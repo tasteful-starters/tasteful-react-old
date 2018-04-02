e2d75052979dfff496048d4d7645d53ac97ed25e
Pedro <buesimples@gmail.com>
2018-04-02 18:09:04 +0100

    8. Todo App - 1. Create with basic architecture

--- STEPS.md
+++ STEPS.md
@@ -12,6 +12,14 @@
 
 ## 6. [Routing](./steps/6.Routing.md)
 
-## 7. Setup Redux
+## 7. [Setup Redux](./steps/7.SetupRedux.md)
 
 ## 8. Todo App
+
+### 1. [Create with basic architecture](./steps/todo-app/1.CreateWithBasicArchitecture.md)
+
+### 2. [Replace local state with redux](./steps/todo-app/2.ReplaceLocalStateWithRedux.md)
+
+### 3. [Convert to component based architecture](./steps/todo-app/3.ConvertToComponentBasedArchitecture.md)
+
+### 4. [Routes](./steps/todo-app/4.Routes.md)
--- src/app/todo-app/index.js
+++ src/app/todo-app/index.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ToDo = () => (
-    <h1>ToDo works!</h1>
-);
+class ToDo extends Component {
+
+    constructor(props) {
+        super();    // https://stackoverflow.com/questions/40433463/what-does-calling-super-in-a-react-constructor-do
+
+        this.state = {
+            newTodo: '',
+            list: [
+                { name: 'clean room', done: false },
+                { name: 'make pancakes', done: false },
+                { name: 'spend 3 hours on reddit', done: true }
+            ]
+        }
+    }
+
+    handleToDoCheckBoxChange = (event, i) => {
+        const value = event.target.checked;
+
+        this.setState({ 'list': 
+            [...this.state.list].map((obj, j) => {
+                if (i === j) {
+                    return {
+                        ...obj,
+                        done: value
+                    }
+                }
+                return obj;
+            })
+        })
+    }
+
+    handleNewToDoChange = (event) => {
+        this.setState({newTodo: event.target.value});
+    }
+
+    addTodo = () => {
+        this.setState({ 'list': 
+            [
+                ...this.state.list, {
+                    name: this.state.newTodo,
+                    done: false
+                }
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>ToDo works!</h1>
+
+                <div>
+                    <input
+                        type="text"
+                        value={this.state.newTodo}
+                        onChange={this.handleNewToDoChange}
+                    />
+                    <button onClick={this.addTodo}>Add ToDo</button>
+                </div>
+
+                <ul>
+                    {this.state.list.map((obj, i) => (
+                        <li key={i}>
+                            <input
+                                name={i}
+                                type="checkbox"
+                                checked={obj.done}
+                                onChange={ e => this.handleToDoCheckBoxChange(e, i) }
+                            />
+                            {obj.name}
+                        </li>)
+                    )}
+                </ul>
+
+                <hr />
+                <h2>Debug Stuff</h2>
+                <pre>{JSON.stringify(this.state.list, null, 2)}</pre>
+            </div>
+        );
+    }
+}
 
 export default ToDo;
